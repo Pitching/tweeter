7599b047cf46f0991d157a33f9e85703f7b62a83
Andrei Chirita <adchir95@gmail.com>
2022-10-12 21:09:04 +0000

    updating the createTweetElement to take text to avoid any script injections

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,27 @@
 const createTweetElement = function(tweet) {
-  const $tweet = `
-  <article class="tweet">
-    <header class="header-tweet">
-      <img id=image src =${tweet.user.avatars}/><h3 id="name">${tweet.user.name}</h3>
-      <h3 id="handle">${tweet.user.handle}</h3>
-    </header>
-  <p>${tweet.content.text}</p>
-    <footer class="footer-tweet">
-      <section>${timeago.format(tweet.created_at)}</section>
-        <div>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>
-  `;
-  return $tweet;
+
+  const $article = $('<article class="tweet">');
+  const $header = $('<header class="header-tweet">');
+  const $portrait = $(`<img id=image src=${tweet.user.avatars}/>`)
+  const $name = $('<h3 id="name">').text(tweet.user.name);
+  const $handle = $('<h3 id="handle">').text(tweet.user.handle);
+  const $text = $('<p>').text(tweet.content.text);
+  const $footer = $('<footer class="footer-tweet">')
+  const $section = $('<section>').text(timeago.format(tweet.created_at));
+  const $icons = $('<div>').append('<i class="fa-solid fa-flag"></i>', '<i class="fa-solid fa-retweet"></i>', '<i class="fa-solid fa-heart"></i>');
+
+  $header.append($portrait, $name, $handle);
+  $footer.append($section, $icons);
+  $article.append($header, $text, $footer);
+
+  return $article;
 }
 
 const renderTweets = function(tweets) {
+  $("#tweets-container").empty();
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+    $('#tweets-container').prepend($tweet);
   }
 }
 
@@ -36,17 +35,20 @@ $(document).ready(function () {
   };
 
   loadTweets();
-
+  
   $('.formTweeterText').submit(function (event) {
     event.preventDefault();
-    const $tweeterString = $(this).serialize();
-    if ($tweeterString.slice(5) === "" || $tweeterString.slice(5) === null) {
+    const $serializedTweet = $(this).serialize();
+    if ($serializedTweet.slice(5) === "" || $serializedTweet.slice(5) === null) {
       return alert('There is no content in the tweet text!');
-    } else if ($tweeterString.slice(5).length > 140) {
+    } else if ($serializedTweet.slice(5).length > 140) {
       return alert('Your tweet is too long!');
     }
-    console.log($tweeterString);
-    $.post('/tweets', $tweeterString);
+    console.log($serializedTweet);
+    $.post('/tweets', $serializedTweet, () => {
+      $(".counter").text("140");
+      $("#tweet-text").val("");
+      loadTweets();
+    });
   });
-
 });
