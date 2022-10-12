5f696e0163e4935bf4c827b6942b19d0eca45f07
Andrei Chirita <adchir95@gmail.com>
2022-10-12 19:48:46 +0000

    changing avatar to match the images from DB and adding ajax to post and get requests for the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,47 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = function(tweet) {
+  const $tweet = `
+  <article class="tweet">
+    <header class="header-tweet">
+      <img id=image src =${tweet.user.avatars}/><h3 id="name">${tweet.user.name}</h3>
+      <h3 id="handle">${tweet.user.handle}</h3>
+    </header>
+  <p>${tweet.content.text}</p>
+    <footer class="footer-tweet">
+      <section>${timeago.format(tweet.created_at)}</section>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `;
+  return $tweet;
+}
 
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+}
+
+$(document).ready(function () {
+
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+
+  $('.formTweeterText').submit(function (event) {
+    event.preventDefault();
+    const $tweeterString = $(this).serialize()
+    console.log($tweeterString);
+    $.post('/tweets', $tweeterString);
+  });
+
+});
